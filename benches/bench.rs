284abea2242390c937aaa057ca578952dd90f384
Sho Nakatani <lay.sakura@gmail.com>
2019-05-03 09:39:59 +0900

    predictive_search() bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate criterion;
 
+#[macro_use]
+extern crate lazy_static;
+
 use criterion::Criterion;
 use std::time::Duration;
 
@@ -22,19 +25,72 @@ fn git_hash() -> String {
 
 mod trie {
     use criterion::{BatchSize, Criterion};
+    use std::env;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    use std::str;
+    use trie_rs::{Trie, TrieBuilder};
 
-    const NS: [u64; 5] = [1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15];
+    lazy_static! {
+        // Construct Japanese dictionary using EDICT (http://www.edrdg.org/jmdict/edict.html).
+        static ref TRIE_EDICT: Trie<u8> = {
+            let mut builder = TrieBuilder::new();
 
-    pub fn to_be_removed_benchmark(_: &mut Criterion) {
-        let times = 10_000;
+            let repo_root = env::var("REPO_ROOT").expect("REPO_ROOT environment variable must be set.");
+            let edict2_path = format!("{}/benches/edict.furigana", repo_root);
+            println!("Reading dictionary file from: {}", edict2_path);
+            for result in BufReader::new(File::open(edict2_path).unwrap()).lines() {
+                let l = result.unwrap();
+                builder.push(l);
+            }
+            builder.build()
+            // TODO print heap usage
+        };
+    }
 
-        super::c().bench_function_over_inputs(
-            &format!("[{}] 1+1 {} times", super::git_hash(), times,),
-            move |b, &&_n| b.iter_batched(|| (), |_| 1 + 1, BatchSize::SmallInput),
-            &NS,
+    pub fn predictive_search(_: &mut Criterion) {
+        let times = 100;
+
+        super::c().bench_function(
+            &format!(
+                "[{}] Trie::predictive_match() {} times",
+                super::git_hash(),
+                times
+            ),
+            move |b| {
+                b.iter_batched(
+                    || &TRIE_EDICT,
+                    |trie| {
+                        // iter_batched() does not properly time `routine` time when `setup` time is far longer than `routine` time.
+                        // Tested function takes too short compared to build(). So loop many times.
+                        let results_in_u8s = trie.predictive_search("すし");
+                        for _ in 0..(times - 1) {
+                            trie.predictive_search("すし");
+                        }
+
+                        let results_in_str: Vec<&str> = results_in_u8s
+                            .iter()
+                            .map(|u8s| str::from_utf8(u8s).unwrap())
+                            .collect();
+                        assert_eq!(
+                            results_in_str,
+                            vec![
+                                "すし",
+                                "すしだね",
+                                "すしづめ",
+                                "すしのぐ",
+                                "すしめし",
+                                "すしや",
+                                "すしをにぎる"
+                            ]
+                        );
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
     }
 }
 
-criterion_group!(benches, trie::to_be_removed_benchmark);
+criterion_group!(benches, trie::predictive_search);
 criterion_main!(benches);
