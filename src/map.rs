ac7b9b9d7364a16e696a608816ebd23df9768b51
Shane Celis <shane.celis@gmail.com>
2024-02-10 04:47:57 -0500

    doc: Update README; add map usage.

--- src/map.rs
+++ src/map.rs
@@ -55,6 +55,7 @@ impl<K: Clone, V: Clone> Trie<K,V> where KeyValue<K,V>: Ord + Clone {
         self.inner.common_prefix_search(q).into_iter().map(|v| Self::strip(v)).collect()
     }
 
+    /// Given a list of `KeyValue`s take the last value and return only the keys.
     fn strip(mut word: Vec<KeyValue<K,V>>) -> (Vec<K>, V) {
         let value = word.last_mut().expect("word should have length > 0").1.clone().expect("Terminal node should have value");
         (word.into_iter().map(|x| x.0).collect(), value)
