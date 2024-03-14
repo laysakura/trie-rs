1131ec8a1a85425368461670edb81fd3d3a7a50f
Shane Celis <shane.celis@gmail.com>
2024-03-13 23:04:29 -0400

    refactor: Move tests.

--- src/trie.rs
+++ src/trie.rs
@@ -130,3 +130,223 @@ impl<Label: Ord + Clone> TrieBuilder<Label> {
         }
     }
 }
+
+#[cfg(test)]
+mod search_tests {
+    use crate::{Trie, TrieBuilder};
+
+    fn build_trie() -> Trie<u8> {
+        let mut builder = TrieBuilder::new();
+        builder.push("a");
+        builder.push("app");
+        builder.push("apple");
+        builder.push("better");
+        builder.push("application");
+        builder.push("アップル🍎");
+        builder.build()
+    }
+
+    mod exact_match_tests {
+        macro_rules! parameterized_tests {
+            ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (query, expected_match) = $value;
+                    let trie = super::build_trie();
+                    let result = trie.exact_match(query);
+                    assert_eq!(result, expected_match);
+                }
+            )*
+            }
+        }
+
+        parameterized_tests! {
+            t1: ("a", true),
+            t2: ("app", true),
+            t3: ("apple", true),
+            t4: ("application", true),
+            t5: ("better", true),
+            t6: ("アップル🍎", true),
+            t7: ("appl", false),
+            t8: ("appler", false),
+        }
+    }
+
+    mod is_prefix_tests {
+        macro_rules! parameterized_tests {
+            ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (query, expected_match) = $value;
+                    let trie = super::build_trie();
+                    let result = trie.is_prefix(query);
+                    assert_eq!(result, expected_match);
+                }
+            )*
+            }
+        }
+
+        parameterized_tests! {
+            t1: ("a", true),
+            t2: ("app", true),
+            t3: ("apple", false),
+            t4: ("application", false),
+            t5: ("better", false),
+            t6: ("アップル🍎", false),
+            t7: ("appl", true),
+            t8: ("appler", false),
+            t9: ("アップル", true),
+            t10: ("ed", false),
+            t11: ("e", false),
+            t12: ("", true),
+        }
+    }
+
+    mod predictive_search_tests {
+        macro_rules! parameterized_tests {
+            ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (query, expected_results) = $value;
+                    let trie = super::build_trie();
+                    let results = trie.predictive_search(query).into_iter().map(|g| String::from_utf8(g).unwrap()).collect::<Vec<_>>();
+                    // results.sort_by(|a, b| a.len().cmp(&b.len()));
+                    assert_eq!(results, expected_results);
+                }
+            )*
+            }
+        }
+
+        parameterized_tests! {
+            t1: ("a", vec!["a", "app", "apple", "application"]),
+            t2: ("app", vec!["app", "apple", "application"]),
+            t3: ("appl", vec!["apple", "application"]),
+            t4: ("apple", vec!["apple"]),
+            t5: ("b", vec!["better"]),
+            t6: ("c", Vec::<&str>::new()),
+            t7: ("アップ", vec!["アップル🍎"]),
+        }
+    }
+
+    mod common_prefix_search_tests {
+        macro_rules! parameterized_tests {
+            ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (query, expected_results) = $value;
+                    let trie = super::build_trie();
+                    let results = trie.common_prefix_search(query);
+                    let expected_results: Vec<Vec<u8>> = expected_results.iter().map(|s| s.as_bytes().to_vec()).collect();
+                    assert_eq!(results, expected_results);
+                }
+            )*
+            }
+        }
+
+        parameterized_tests! {
+            t1: ("a", vec!["a"]),
+            t2: ("ap", vec!["a"]),
+            t3: ("appl", vec!["a", "app"]),
+            t4: ("appler", vec!["a", "app", "apple"]),
+            t5: ("bette", Vec::<&str>::new()),
+            t6: ("betterment", vec!["better"]),
+            t7: ("c", Vec::<&str>::new()),
+            t8: ("アップル🍎🍏", vec!["アップル🍎"]),
+        }
+    }
+
+    mod posfix_search_tests {
+
+        #[test]
+        fn postfix_unfused() {
+            let trie = super::build_trie();
+            let postfixes = trie.postfix_search_ref("app");
+            let mut iter = postfixes.into_inner().map(|x| *x as char);
+            assert_eq!(iter.next(), Some('p'));
+            assert!(iter.next().is_none());
+            assert_eq!(iter.next(), Some('p'));
+            assert_eq!(iter.next(), Some('l'));
+            assert_eq!(iter.next(), Some('e'));
+        }
+
+        #[test]
+        fn postfix_baseline() {
+            let trie = super::build_trie();
+            let postfixes = trie.postfix_search_ref("app");
+            let mut chunks = postfixes.into_iter();
+            // assert_eq!(chunks.by_ref().count(), 3);
+            let mut iter = chunks.next().unwrap().map(|x| *x as char);
+            assert_eq!(iter.next(), Some('p'));
+            assert_eq!(iter.next(), None);
+            let mut iter = chunks.next().unwrap().map(|x| *x as char);
+            assert_eq!(iter.next(), Some('p'));
+            assert_eq!(iter.next(), Some('l'));
+            assert_eq!(iter.next(), Some('e'));
+            assert_eq!(iter.next(), None);
+            let mut iter = chunks.next().unwrap().map(|x| *x as char);
+            assert_eq!(iter.next(), Some('p'));
+            assert_eq!(iter.next(), Some('l'));
+            assert_eq!(iter.next(), Some('i'));
+            assert_eq!(iter.next(), Some('c'));
+            assert_eq!(iter.next(), Some('a'));
+            assert_eq!(iter.next(), Some('t'));
+            assert_eq!(iter.next(), Some('i'));
+            assert_eq!(iter.next(), Some('o'));
+            assert_eq!(iter.next(), Some('n'));
+            assert_eq!(iter.next(), None);
+
+            assert!(chunks.next().is_none());
+        }
+
+        #[test]
+        fn postfix_2() {
+            let trie = super::build_trie();
+            let postfixes = trie.postfix_search_ref("b");
+            let mut chunks = postfixes.into_iter();
+            let mut iter = chunks.next().unwrap().map(|x| *x as char);
+            assert_eq!(iter.next(), Some('b'));
+            assert_eq!(iter.next(), Some('e'));
+            assert_eq!(iter.next(), Some('t'));
+            assert_eq!(iter.next(), Some('t'));
+            assert_eq!(iter.next(), Some('e'));
+            assert_eq!(iter.next(), Some('r'));
+            assert_eq!(iter.next(), None);
+            assert!(chunks.next().is_none());
+        }
+
+        #[test]
+        fn postfix_3() {
+            let trie = super::build_trie();
+            let postfixes = trie.postfix_search_ref("bet");
+            let mut chunks = postfixes.into_iter();
+            let mut iter = chunks.next().unwrap().map(|x| *x as char);
+            assert_eq!(iter.next(), Some('t'));
+            assert_eq!(iter.next(), Some('t'));
+            assert_eq!(iter.next(), Some('e'));
+            assert_eq!(iter.next(), Some('r'));
+            assert_eq!(iter.next(), None);
+            assert!(chunks.next().is_none());
+        }
+
+        #[test]
+        fn postfix_no_match() {
+            let trie = super::build_trie();
+            let postfixes = trie.postfix_search("NOT-THERE");
+            let chunks = postfixes.into_iter();
+            assert_eq!(chunks.count(), 0);
+        }
+
+        #[test]
+        fn vec_into_iter_clone() {
+            let v = vec![1, 2, 3];
+            let i = v.into_iter();
+            let c = i.clone();
+            assert_eq!(c.count(), 3);
+            assert_eq!(i.count(), 3);
+        }
+    }
+}
