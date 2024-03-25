5cfeeb7d14a44c5d7aa502a22afb6ed0af0a6958
Shane Celis <shane.celis@gmail.com>
2024-03-25 06:35:08 -0400

    excise: Remove old clone-less iterators.

--- src/map.rs
+++ src/map.rs
@@ -2,16 +2,9 @@ use crate::internal_data_structure::naive_trie::NaiveTrie;
 use louds_rs::Louds;
 
 mod longest_prefix_iter;
-mod postfix_iter;
-mod prefix_iter;
-mod search_iter;
 mod trie;
 mod trie_builder;
 
-pub use longest_prefix_iter::LongestPrefixIter;
-pub use postfix_iter::PostfixIter;
-pub use prefix_iter::PrefixIter;
-pub use search_iter::SearchIter;
 pub mod clone;
 
 /// A trie for sequences of the type `Label`; each sequence has an associated `Value`.
