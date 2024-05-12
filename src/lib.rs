89c8a67a02792a21a932c807316da77d8973da7d
Shane Celis <shane.celis@gmail.com>
2024-05-12 15:43:21 -0400

    chore: Bump to version 0.4.2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![forbid(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/trie-rs/0.4.1")]
+#![doc(html_root_url = "https://docs.rs/trie-rs/0.4.2")]
 #![doc = include_str!("../README.md")]
 
 pub mod inc_search;
